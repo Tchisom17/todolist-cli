20521b6217bb0f5674c60984c476acc2a8a05dea
Tchisom17 <kamsiyochukwu1@gmail.com>
2022-05-19 19:23:06 +0100

    Remove unnecessary line of code

--- cmd/list.go
+++ cmd/list.go
@@ -1,10 +1,8 @@
 package cmd
 
 import (
-	"github.com/tchisom17/todolist-cli/utils"
-	"log"
-
 	"github.com/spf13/cobra"
+	"github.com/tchisom17/todolist-cli/utils"
 )
 
 // listCmd represents the list command
@@ -13,7 +11,6 @@ var listCmd = &cobra.Command{
 	Short: "Display a list of all tasks",
 	Long: `Display a list of all tasks`,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("list called")
 		utils.Todo.List()
 	},
 }
